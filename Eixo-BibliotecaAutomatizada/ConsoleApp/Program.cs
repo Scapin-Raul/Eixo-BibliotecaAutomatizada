7453bac659c12b7dd4b96388f2fdc6022ba2bea9
Scapin-Raul <raul.scapin19@gmail.com>
2020-08-18 10:37:39 -0300

    Inicialização do projeto com pastas iniciais

---
+++ Eixo-BibliotecaAutomatizada/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Dominios.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        List<Alunos> ListaDeAlunos = new List<Alunos>();
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu RM:");
+            var RmAluno = Console.ReadLine();
+
+            if (RmAluno == "1313")
+            {
+
+                Console.WriteLine("Insira sua senha:");
+                var SenhaAluno = Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Rm inválido");
+
+            }
+        
+        
+        }
+    }
+}
---
+++ Eixo-BibliotecaAutomatizada/Dominios/Classes/Alunos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Dominios.Classes
+{
+    public class Alunos
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Nome", TypeName = "varchar(255)")]
+        public string Nome { get; set; }
+
+        [Required]
+        [Column("Senha", TypeName = "varchar(255)")]
+        public string Senha{ get; set; }
+
+        
+        //[ForeignKey("Produtos")]
+        //[Column("IdLivroEmprestado", TypeName = "int")]
+        //public int IdLivroEmprestado { get; set; }
+        //public Livros Livro { get; set; }
+
+    }
+}
